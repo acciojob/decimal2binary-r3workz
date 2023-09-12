c19bc7400ec14e8e7c3e29a21245d84fa634b841
r3workz <122603849+r3workz@users.noreply.github.com>
2023-09-12 21:44:18 +0000

    attempt1

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function decimalToBinary(num) {
-  // Write your code here
-}
+const input = document.getElementById("decToBin")
+const output = document.getElementById("binaryText")
 
-window.decimalToBinary = decimalToBinary;
+function decimalToBinary(event) {
+  // Write your code here
+  let num = event.target.value
+  let rem = ""
+  while(num>0){
+    rem = num%2 + rem
+    num=Math.floor(num/2)
+  }
+  output.innerText = `${rem}`
+}
+input.addEventListener("change",decimalToBinary)
+
+// window.decimalToBinary = decimalToBinary;
